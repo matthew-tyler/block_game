5d7d9e951c8cd034e8da970131c3271d670d2b16
Matt Tyler <tylma141@student.otago.ac.nz>
2024-01-11 21:05:26 +1100

    Scoring and row clearing added

--- block_game.py
+++ block_game.py
@@ -21,7 +21,7 @@ class BlockGame:
         COLOURS = ["\033[31m", "\033[32m", "\u001b[33m", "\u001b[34m",
                    "\u001b[35m", "\u001b[36m", "\033[1;34m", "\033[1;31m"]
         RESET = "\033[0m"
-
+        print(self.score, self.failed_turns)
         for row in self.board:
             row_str = ' '.join(
                 f"{COLOURS[cell - 1]}■{RESET}" if cell > 0 else "□" for cell in row)
@@ -52,6 +52,51 @@ class BlockGame:
                     return False
         return True
 
+    def count_non_zero(self, lst):
+
+        non_zero_count = 0
+        # Check if the first element of lst is a list (indicating it's a 2D list)
+        if lst and isinstance(lst[0], list):
+            for row in lst:
+                for cell in row:
+                    if cell != 0:
+                        non_zero_count += 1
+        else:
+            # If it's a 1D list, loop through each element
+            for element in lst:
+                if element != 0:
+                    non_zero_count += 1
+
+        return non_zero_count
+
+    def score_board(self):
+
+        rows_to_clear = []
+        cols_to_clear = []
+
+        for row in range(self.size):
+            filled_cells = self.count_non_zero(self.board[row])
+
+            if filled_cells == self.size:
+                rows_to_clear.append(row)
+
+        for col in range(self.size):
+            column = [self.board[row][col] for row in range(self.size)]
+
+            filled_cells = self.count_non_zero(column)
+            if filled_cells == self.size:
+                cols_to_clear.append(col)
+
+        num_to_clear = len(rows_to_clear) + len(cols_to_clear)
+        self.score += num_to_clear * (num_to_clear + 1) // 2 * 100
+
+        for row in rows_to_clear:
+            self.board[row] = [0] * self.size
+
+        for col in cols_to_clear:
+            for row in range(self.size):
+                self.board[row][col] = 0
+
     def place_piece(self, piece, anchor_x, anchor_y):
         piece_center = len(piece) // 2
         for i in range(len(piece)):
@@ -73,8 +118,9 @@ class BlockGame:
             self.place_piece(piece, x, y)
             # replace used piece
             self.current_pieces[piece_number] = self.random_piece()
-            # need to update the score. Not sure by what value.
-            self.score += 0
+            # Score increase by 10 for each sub block fo a piece.
+            self.score += self.count_non_zero(piece) * 10
+            self.score_board()
         else:
             self.failed_turns += 1
 
@@ -85,7 +131,8 @@ class BlockGame:
 new_game = BlockGame()
 
 
-for i in range(0, 10):
-    new_game.make_move(1, (4 + i) % 11, (5 + i) % 11)
+for i in range(0, 2000):
+    new_game.make_move(1, (random.randint(0, 10) + i) %
+                       11, (random.randint(0, 20) + i) % 11)
 
 new_game.display_board()
